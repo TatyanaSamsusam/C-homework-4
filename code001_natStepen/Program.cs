bd9bac63bf8b84fafe3e05114d998bdde2b56377
Tatyana <android87@list.ru>
2022-03-25 22:55:45 +0300

    made nat stepen

---
+++ code001_natStepen/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.WriteLine($"Введи 2 числа, и я возведу первое число в степень второго: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+double stepen = Math.Pow(numberA, numberB);
+Console.WriteLine($"{numberA} в степени {numberB} это {stepen}");
+
